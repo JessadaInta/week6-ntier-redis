72d2fd3a617e9b0d5a84d22f5ee13c77fd785ea8
JessadaInta <nnicenicekung@gmail.com>
2026-02-23 10:37:25 +0700

    deleted x cache

--- api/src/routes/taskRoutes.js
+++ api/src/routes/taskRoutes.js
@@ -1,19 +1,13 @@
+// src/routes/taskRoutes.js
 const express = require('express');
 const router = express.Router();
 const taskController = require('../controllers/taskController');
-const cacheMiddleware = require('../middleware/cacheMiddleware');
 
-router.get('/stats', taskController.getStatistics);
-router.get('/', taskController.getAllTasks);
-
-router.get(
-  '/:id(\\d+)',
-  cacheMiddleware((req) => `tasks:${req.params.id}`),
-  taskController.getTaskById
-);
-
-router.post('/', taskController.createTask);
-router.put('/:id(\\d+)', taskController.updateTask);
-router.delete('/:id(\\d+)', taskController.deleteTask);
+router.get('/tasks', (req, res, next) => taskController.getAllTasks(req, res, next));
+router.get('/tasks/stats', (req, res, next) => taskController.getStatistics(req, res, next));
+router.get('/tasks/:id', (req, res, next) => taskController.getTaskById(req, res, next));
+router.post('/tasks', (req, res, next) => taskController.createTask(req, res, next));
+router.put('/tasks/:id', (req, res, next) => taskController.updateTask(req, res, next));
+router.delete('/tasks/:id', (req, res, next) => taskController.deleteTask(req, res, next));
 
 module.exports = router;
